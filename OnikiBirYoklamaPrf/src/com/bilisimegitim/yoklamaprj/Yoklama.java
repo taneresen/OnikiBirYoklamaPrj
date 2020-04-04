320b339e4be948a5894d7eca1c2f745142a5cee6
taner <taner@MSI>
2020-04-04 20:17:08 +0200

    Ilk Classimiz Yoklama Java Eklendi.

---
+++ OnikiBirYoklamaPrf/src/com/bilisimegitim/yoklamaprj/Yoklama.java
@@ -0,0 +1,20 @@
+package com.bilisimegitim.yoklamaprj;
+
+public class Yoklama {
+
+	public static void main(String[] args) {
+		
+		
+		String[] isimler = {"Taner","Mustafa","Mehmet","Nihan","Asya"};
+		
+		for (int i = 0; i < isimler.length; i++) {
+			
+			System.out.println(isimler[i]);
+			
+		}
+		
+		
+
+	}
+
+}
